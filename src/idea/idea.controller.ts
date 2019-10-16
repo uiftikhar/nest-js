98defcae857c1d3371c5b90fbec7c332fdffd026
Umer Iftikhar <umer229@gmail.com>
2019-10-16 18:34:26 +0200

    add database integration with postgres (#1)
    * add database integration with postgres

---
+++ src/idea/idea.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { IdeaService } from './idea.service';
+import { IdeaDto } from './idea.dto';
+
+@Controller('idea')
+export class IdeaController {
+  constructor(private readonly ideaService: IdeaService) {}
+
+  @Get()
+  showAllIdeas() {
+    return this.ideaService.showAll();
+  }
+
+  @Post()
+  createIdea(@Body() data: IdeaDto) {
+    return this.ideaService.create(data);
+  }
+
+  @Get(':id')
+  readIdea(@Param('id') id: string) {
+    return this.ideaService.readIdea(id);
+  }
+
+  @Put(':id')
+  updateIdea(@Param('id') id: string, @Body() data: Partial<IdeaDto>) {
+    return this.ideaService.update(id, data);
+  }
+
+  @Delete(':id')
+  deleteIdea(@Param('id') id: string) {
+    return this.ideaService.destroy(id);
+  }
+}
