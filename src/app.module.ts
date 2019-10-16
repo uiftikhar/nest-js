4ab7a00a65159e7909a257bab34597282da4d1bd
umer <umer229@gmail.com>
2019-10-16 19:27:59 +0200

    WIP

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GraphQLModule } from '@nestjs/graphql';
-import { CatsModule } from './cats/cats.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { IdeaModule } from './idea/idea.module';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpErrorFilter } from './shared/http-error.filter';
 
 @Module({
-  imports: [
-    // CatsModule,
-    // GraphQLModule.forRoot({
-    //   autoSchemaFile: 'schema.gql',
-    // }),
-    // MongooseModule.forRoot('mongodb://localhost/nest'),
-    TypeOrmModule.forRoot(),
-    IdeaModule,
-  ],
+  imports: [TypeOrmModule.forRoot(), IdeaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/shared/logging.interceptor.ts
@@ -0,0 +1,33 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  Logger,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Request, Response } from 'express';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler
+  ): Observable<any> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const { method, url } = request;
+    const now = Date.now();
+
+    return next
+      .handle()
+      .pipe(
+        tap(() =>
+          Logger.log(
+            `${method} ${url} ${Date.now() - now}ms`,
+            context.getClass().name
+          )
+        )
+      );
+  }
+}
